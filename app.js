99093d74ed599fb18aaaf0170f3565c61f8454e0
Kongpob7k <kongpopteen@gmail.com>
2025-07-17 00:34:00 +0700

    Deploy Commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("node:path");
 const express = require("express");
 const app = express();
 const indexRouter = require("./routes/indexRoute");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
